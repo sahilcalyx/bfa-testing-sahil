c93053b26bcd29726d12d26044738d9b4e5e7196
sahilcalyx <sahilsawantcalyx@gmail.com>
2026-05-29 11:01:23 +0530

    sahil

--- src/react/Pages/TermsAndConditions.jsx
+++ src/react/Pages/TermsAndConditions.jsx
@@ -75,11 +75,11 @@ const TermsAndConditions = () => {
         {/* A) SPONSORSHIP TERMS */}
         {/* ---------------------------------------------------------------------- */}
 
-        <h3>Sponsorship Terms and Conditions</h3>
+        <h3>Sponsorship terms and conditions:</h3>
 
         {/* GENERAL */}
-        <h4>General</h4>
-        <ol>
+        <h4>General:</h4>
+        <ol style={{ listStyleType: "decimal", paddingLeft: "2rem" }}>
           <li>
             Sponsorship amount is due upon filling up the form and must be paid in full within one week of signing the Sponsorship Agreement. Payment methods accepted include bank transfer and credit card.
           </li>
@@ -89,13 +89,12 @@ const TermsAndConditions = () => {
           <li>
             In the event of sponsorship cancellation, refunds will be provided according to the following schedule:
             <ul>
-              <li>14 days cooling-off period, 10% administration fee deducted.</li>
+              <li>14 days cooling off period, 10% administration fee will be deducted.</li>
               <li>45 days before the event, 50% refund.</li>
               <li>Less than 45 days to the event, no refunds offered.</li>
             </ul>
           </li>
-          <li>Cancellations must be made in writing and sent to{" "}
-            <a href="mailto:kudos@britfintechawards.com">kudos@britfintechawards.com</a>.</li>
+          <li>Cancellations must be made in writing and sent to <a href="mailto:kudos@britfintechawards.com">kudos@britfintechawards.com</a>.</li>
           <li>
             The Brit Fintech Awards reserve the right to terminate the sponsorship agreement if the sponsor fails to comply with these terms and conditions, without refunding any fees.
           </li>
@@ -108,39 +107,41 @@ const TermsAndConditions = () => {
         </ol>
 
         {/* DIGITAL BRANDING */}
-        <h4>Digital Branding</h4>
+        <h4>DIGITAL BRANDING:</h4>
 
-        <h5>I) Website Presence</h5>
-        <ol>
+        <h5>I) Website Presence:</h5>
+        <ol style={{ listStyleType: "decimal", paddingLeft: "2rem" }}>
           <li>
-            Company Logo & Criteria: Company Logo, in the required size and format as .CDR/Adobe Illustrator/ Figma, must be submitted post-sponsorship confirmation within one week.
+            Company Logo & Criteria: Company Logo, in the required size and format, must be submitted post-sponsorship confirmation within one week.
           </li>
           <li>
-            Content Submission: Sponsors must submit their logos and informative content about their business in the required format within one week after confirming the sponsorship for inclusion on the website.
+            Content Submission: Sponsors must submit their logos and informative content about their business in the required format within not more than one week after confirming the sponsorship for inclusion on the website.
           </li>
-          <li>The sponsor's logo on the website will remain active after 1 day from the date of logo submission. </li>
-          <li>The sponsor's content on the website will remain active after 1 week from the date of content submission.</li>
+          <li>
+            The Sponsor’s logo will be updated on the BFA website within 1 day after receipt of the logo submission and sponsorship payment. Thereafter, the Sponsor’s logo and company profile will remain on the BFA website forever.
+          </li>
         </ol>
 
-        <h5>II) Video Branding</h5>
+        <h5>II) Video Branding:</h5>
         <p>
-          Sponsors must provide their logo in the .CDR/Adobe Illustrator/ Figma format and high resolution within one week to include in event-related videos which will be initiated after the payment is processed.
+          Sponsors must provide their logo in the .CDR format and high resolution within one week to include in event-related videos which will be initiated after the payment is processed.
         </p>
 
-        <h5>III) Email Branding</h5>
-        <p> The logo will be featured in all BFA event-related newsletters once the sponsorship payment is completed.</p>
+        <h5>III) Email Branding:</h5>
+        <p>
+          The logo will be featured in all BFA event-related newsletters once the sponsorship payment is completed.
+        </p>
 
-        <h5>IV) Social Media Branding</h5>
-        <ol>
+        <h5>IV) Social Media Branding:</h5>
+        <ol style={{ listStyleType: "decimal", paddingLeft: "2rem" }}>
           <li>
-            Marketing plan including the topics to be covered and the associated timelines, should be finalised to be shared 3 months prior to the event or 1 week after the confirmation of the sponsorship; whichever is applicable.
+            Marketing plan including the topics to be covered and the associated timelines, should be finalised to be shared 2 months prior to the event or 1 week after the confirmation of the sponsorship; whichever is applicable.
           </li>
-          <li> Content Usage: By submitting materials, sponsors grant the right to use content for promotional purposes.</li>
-
+          <li>Content Usage: By submitting materials, sponsors grant the right to use content for promotional purposes.</li>
           <li>
-            Social Media Plan:
+            Social media plan:
             <ul style={{ listStyleType: "upper-alpha", paddingLeft: "2rem" }}>
-              <li>Platinum Sponsor: 8 Social media promotion - 4 Paid Ad post total reach count 5,000 - 20,000 </li>
+              <li>Platinum Sponsor: 8 Social media promotion - 5 Paid Ad post total reach count 5,000 - 20,000</li>
               <li>Gold Sponsor: 5 Social media promotion- 3 Paid Ad post total reach count 2,000 - 15,000</li>
               <li>Silver Sponsor: 3 Social media promotion- 2 Paid Ad post total reach count 1,000 - 10,000</li>
             </ul>
@@ -151,7 +152,7 @@ const TermsAndConditions = () => {
             Out of the agreed number of posts, one post will be of sponsorship announcement; and another can be a winner post (in case you win).
           </li>
           <li>
-            PAID Ad posts will be run on the YouTube and Instagram.
+            PAID Ad posts will be run on the LinkedIn and Instagram.
           </li>
           <li>
             This will start once the sponsorship payment is done and the marketing plan is shared and agreed.
@@ -161,77 +162,78 @@ const TermsAndConditions = () => {
           </li>
         </ul>
 
-        <h5>V) Newsletters</h5>
-        <ul style={{ listStyleType: "decimal" }}>
-          <li>Sponsors must submit their content and any marketing material within one week to ensure its included in the newsletters.</li>
-          <li>Newsletter topic to be provided by the Sponsor within one week. Team can help in suggesting topics but final desicion will be with the Sponsor.</li>
-          <li> Out of the agreed number of newsletters, one newsletter will be of sponsorship announcement; and another can be a winner announcement (in case you win).
-          </li>
+        <h5>V) Newsletters:</h5>
+        <ul style={{ listStyleType: "lower-alpha", paddingLeft: "2rem" }}>
+          <li>Sponsors must submit their content and any marketing material within agreed timeframe of one week to ensure its included in the newsletters.</li>
+          <li>Newsletter topic to be provided by the Sponsor within one week. Team can help in suggesting topics but final decision will be with the Sponsor.</li>
+          <li>Out of the agreed number of newsletters, one newsletter will be of sponsorship announcement; and another can be a winner announcement (in case you win).</li>
         </ul>
 
-        {/* PRINT MEDIA */}
-        <h4>Print Media Branding</h4>
-        <ul style={{ listStyleType: "decimal" }}>
+        <h5>VI) Magazine Feature:</h5>
+        <p>
+          Sponsors must provide all required content, including text and images, by the specified deadline to ensure it is included in the magazine.
+        </p>
+
+        {/* PRINT MEDIA BRANDING */}
+        <h4>PRINT MEDIA BRANDING:</h4>
+        <ul style={{ listStyleType: "upper-roman", paddingLeft: "2rem" }}>
           <li>
-            Sponsors must provide their company logo in the .CDR/Adobe Illustrator/ Figma format and high resolution within one week post sponsorship confirmation.
+            Sponsors must provide their company logo in the .CDR format and high resolution within one week post sponsorship confirmation.
           </li>
           <li>
             Company Logo & Criteria: Company Logo and required criteria for printing the logo must be submitted post-sponsorship confirmation within one week.
           </li>
-          <li>The company logo will be featured on the event venue signage, digital display, event schedule, any event registration material and the thankyou note given to all the event attendees </li>
-          <ul>
-            <li>Platinum: One individual pull-up banner per sponsor.</li>
-            <li>Gold: Shared pull-up banners; max 2 sponsors per banner.</li>
-            <li>Silver: Shared pull-up banners; max 4 sponsors per banner.</li>
-          </ul>
+          <li>
+            The company logo will be featured on the event venue signage, digital display, event schedule, any event registration material and the thankyou note given to all the event attendees
+          </li>
+          <li>
+            Platinum: One individual pull-up banner or event space wall branding per sponsor.
+            <br />
+            Gold: Shared pull-up banners; max 2 sponsors per banner.
+            <br />
+            Silver: Shared pull-up banners; max 4 sponsors per banner.
+          </li>
           <li>
             As Platinum sponsors receive first priority and Gold sponsors receive second priority, placement will depend on the number of sponsors and available lanyard space.
           </li>
         </ul>
 
         {/* AWARDS NIGHT BRANDING */}
-        <h4>Awards Night Branding</h4>
+        <h4>AWARDS NIGHT BRANDING:</h4>
 
-        <h5>I) Exhibit Space</h5>
-        <ul>
+        <h5>I) Exhibit Space:</h5>
+        <ol style={{ listStyleType: "decimal", paddingLeft: "2rem" }}>
           <li>Exhibit space selection will be prioritised based on sponsorship tier: Platinum sponsors will have first preference, followed by Gold and Silver sponsors.</li>
           <li>If two sponsors fall under the same category (e.g., Gold), the sponsor who completes payment first will receive priority in choosing their exhibit space.</li>
           <li>Sponsors must finalise their preferred exhibit space by the designated deadline communicated in the first week of September.</li>
-          <li>Sponsors are responsible for the setup of their exhibit space according to the schedule provided by the event organisers.</li>
-          <li>Any additional requests should be conveyed by the Sponsor well in advance. The fulfillment of the requests will depend on the logistics available at the finalised venue.
-          </li>
-        </ul>
+          <li>Sponsors are responsible for the setup of their exhibit space according to the schedule provided by the event organizers.</li>
+          <li>Any additional requests should be conveyed by the Sponsor well in advance. The fulfillment of the requests will depend on the logistics available at the finalised venue.</li>
+        </ol>
 
-        <h5>II) Award Sponsorship</h5>
+        <h5>I) Award Sponsorship:</h5>
         <p>Platinum sponsor may choose to sponsor one award of their choice, after checking the availability and seeking approval from the Brit Fintech Awards.</p>
 
-        <h5>III) Key Note Speaker</h5>
-
+        <h5>III) Key Note Speaker:</h5>
         <p>Platinum sponsors are required to provide headshots and details of their keynote speaker, along with the topic of discussion and any relevant slides, atleast 1 week in advance, so we can incorporate them into our presentation flow.</p>
 
-
-        <h5>IV) Discussion Panel</h5>
-        <ul>
-          <li>
-            Sponsors participating as panelists must confirm their availability for the scheduled discussion panel date and time. If the designated panelist is unavailable, an alternate representative should be confirmed to ensure participation.
-          </li>
-          <li>
-            Panelists are required to provide a headshot along with their contact details (name, designation, company name, email address, and contact number) for promotional materials. This information will also be used for important BFA announcements which will be shared via WhatsApp.
-          </li>
+        <h5>IV) Discussion Panel:</h5>
+        <ol style={{ listStyleType: "decimal", paddingLeft: "2rem" }}>
+          <li>Sponsors participating as panelists must confirm their availability for the scheduled discussion panel date and time. If the designated panelist is unavailable, an alternate representative should be confirmed to ensure participation.</li>
+          <li>Panelists are required to provide a headshot along with their contact details (name, designation, company name, email address, and contact number) for promotional materials. This information will also be used for important BFA announcements which will be shared via WhatsApp.</li>
           <li>Panelist information, including headshots, will be used in marketing materials, including social media, newsletters, and the event website.</li>
           <li>If a panelist must cancel their participation, they must notify us in advance to allow for adjustments.</li>
+        </ol>
+
+        <h5>V) Brand Video Clip:</h5>
+        <p>
+          Platinum sponsors must provide video bytes from their designated representative. For video submissions, please ensure the following formats:
+        </p>
+        <ul style={{ listStyleType: "lower-alpha", paddingLeft: "2rem" }}>
+          <li>For screen presentations: Landscape format (16:9) with a resolution of 1920 x 1080 px.</li>
+          <li>For social media reels: Vertical format (9:16) with a resolution of 1080 x 1920 px.</li>
         </ul>
 
-        <h5>V) Brand Video Clip</h5>
-        <ul>
-          <li>Platinum sponsors must provide video bytes from their designated representative. For video submissions, please ensure the following formats:</li>
-          <ul style={{ listStyleType: "lower-alpha" }}>
-            <li>For screen presentations: Landscape format (16:9) with a resolution of 1920 x 1080 px.</li>
-            <li>For social media reels: Vertical format (9:16) with a resolution of 1080 x 1920 px.</li>
-          </ul>
-        </ul>
-
-        <h5>VI) Awards Night Tickets</h5>
+        <h5>VI) Awards Night Tickets:</h5>
         <p>
           Sponsors and Applicants are required to attend all relevant event activities. Need to confirm the event participation along with the participant details 1 month prior to the event date.
         </p>
@@ -242,15 +244,15 @@ const TermsAndConditions = () => {
         {/* B) NOMINATION TERMS */}
         {/* ---------------------------------------------------------------------- */}
 
-        <h3>Nomination Terms and Conditions</h3>
-        <ol style={{ listStyleType: "decimal" }}>
+        <h3>B) Nomination terms and conditions:</h3>
+        <ol style={{ listStyleType: "decimal", paddingLeft: "2rem" }}>
           <li>
-            Once submitted, you get a cooling off period of 14 days to withdraw your nomination and receive a refund deducting 20% administration fee. An entry cannot be withdrawn and refund cannot be claimed after cooling off period is completed.
+            Once submitted, you get a cooling off period of 14 days to withdraw your nomination and receive a refund deducting 20% administration fee. An entry cannot be withdrawn, and refund cannot be claimed after cooling off period is completed.
           </li>
           <li>Nomination payment is due within one week of the invoice date.</li>
           <li>Any incomplete nomination form will be disqualified.</li>
           <li>Maximum of three nominations for the same organisation for different award categories will be accepted.</li>
-          <li> An invoice will be shared to the registered email address of the organisation.</li>
+          <li>An invoice will be shared to the registered email address of the organisation.</li>
           <li>In case you require additional details regarding any Award category then please contact us.</li>
           <li>
             An entry may be disqualified by the Brit FinTech Awards authority if any nomination that does not comply with the terms and conditions or is deemed inappropriate or engages in unethical or fraudulent behaviour.
@@ -259,28 +261,26 @@ const TermsAndConditions = () => {
           <li>
             Nominations will be evaluated based on criteria such as innovation, impact, scalability, and relevance to the fintech industry. The judging panel, composed of experts in fintech and related fields, will review all nominations and select the winners.
           </li>
-          <li> Decisions taken by the Judges Panels in the evaluation process will be final.</li>
+          <li>Decisions taken by the Judges Panels in the evaluation process will be final.</li>
           <li>
             All information submitted as part of the nomination process will be treated as confidential by the BFA team and the judging panel. However, the BFA reserves the right to use Applicants names and basic information for promotional purposes.
           </li>
-          <li> BFA reserves the right to modify or cancel the awards in exceptional circumstances.</li>
+          <li>BFA reserves the right to modify or cancel the awards in exceptional circumstances.</li>
         </ol>
 
-        <h4>Eligibility Criteria</h4>
-        <ul>
-          <li>Annual turnover must exceed £2 million (not applicable for startups, they have a seperate category).</li>
+        <h4>Eligibility Criterias:</h4>
+        <ol style={{ listStyleType: "decimal", paddingLeft: "2rem" }}>
+          <li>Annual turnover must exceed £2 million (not applicable for startups).</li>
           <li>Directors or UBOs must have no criminal charges.</li>
           <li>No penalties from any regulatory body.</li>
           <li>Open to Fintech Companies and MSBs only.</li>
           <li>
-            Supporting documents may be emailed to{" "}
-            <a href="mailto:kudos@britfintechawards.com">kudos@britfintechawards.com</a>.
+            Supporting photos/documents can be sent to <a href="mailto:kudos@britfintechawards.com">kudos@britfintechawards.com</a>.
           </li>
           <li>
-            For registration assistance, contact{" "}
-            <a href="tel:+442038283277">+44 2071939381</a>.
+            For registration assistance, contact us at <a href="tel:+442071939381">+44 2071939381</a>.
           </li>
-        </ul>
+        </ol>
 
         <div className="cs-height_40 cs-height_lg_20" />
 
